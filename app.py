14cf82057b13108405065fe8b9adb9ca13245139
rakeshtds <rakeshtdsharma@yahoo.com>
2024-08-06 17:54:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,6 @@ def main():
     groq_api_key = st.secrets["GROQ_API_KEY"]
     client = Groq(
         api_key=groq_api_key,
-        base_url=st.secrets["GROQ_BASE_URL"]
     )
 
     # Set up the Streamlit interface
